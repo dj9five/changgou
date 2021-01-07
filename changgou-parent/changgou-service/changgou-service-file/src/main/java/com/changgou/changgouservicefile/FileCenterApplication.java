70e329218df3e23caee7bff7e000b00fca78b49a
Gogs <gogs@fake.local>
2021-01-07 22:36:11 +0800

    创建基于fastdfs的文件中心

---
+++ changgou-parent/changgou-service/changgou-service-file/src/main/java/com/changgou/changgouservicefile/FileCenterApplication.java
@@ -0,0 +1,16 @@
+package com.changgou.changgouservicefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//排除掉数据库自动加载
+@EnableEurekaClient
+public class FileCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCenterApplication.class, args);
+    }
+
+}
