6084be6a8a5389e91afdf2a4c24eaa486e0e0ac5
Gogs <gogs@fake.local>
2021-03-15 23:03:27 +0800

    服务网关配置  根据IP进行限流

--- changgou-parent/changgou-gateway/changgou-gateway-web/src/main/java/com/changgou/GatewayWebApplication.java
+++ changgou-parent/changgou-gateway/changgou-gateway-web/src/main/java/com/changgou/GatewayWebApplication.java
@@ -2,7 +2,13 @@ package com.changgou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +18,16 @@ public class GatewayWebApplication {
         SpringApplication.run(GatewayWebApplication.class, args);
     }
 
+    /**
+     * 使用IP作为用户的唯一标识，根据Ip进行限流
+     *
+     * @return
+     */
+    @Bean("ipKeyResolver")
+    public KeyResolver UserKeyResolver() {
+        return exchange -> {
+            String ip = Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getHostString();
+            return Mono.just(ip);
+        };
+    }
 }
